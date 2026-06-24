3d182c12b47262d529cf9463fc3e0d5147b3855b
hyxuu <hyxuu05@gmail.com>
2026-06-23 20:44:18 -0500

    Implementacion de serializadores de salud y rendimiento

--- stats/serializers/__init__.py
+++ stats/serializers/__init__.py
@@ -5,3 +5,11 @@ from .user import (
     UserProfileSerializer,
     ChangePasswordSerializer,
 )
+
+from .contrato import ContratoInternoSerializer
+from .salud import AntecedentesSaludSerializer
+from .antropometria import HistorialAntropometricoSerializer
+from .rendimiento import TestRendimientoSerializer
+from .lesion import LesionRegistroSerializer, SesionRehabilitacionSerializer
+from .dieta import PlanAlimenticioSerializer
+from .prospecto import ProspectoSeguimientoSerializer
